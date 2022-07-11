3424ab82802dacd9bc3d6c597ecc65e6952d1183
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-07-11 18:49:21 -0500

    Scrollbar smooth by sections and partners and footer division

---
+++ src/ChooseUs/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ChooseUs.css";
+
+function ChooseUs() {
+  return (
+    <section className="choose-us">
+      <h1>Choose Us</h1>
+    </section>
+  );
+}
+
+export { ChooseUs };
---
+++ src/KnowUs/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./KnowUs.css";
+
+function KnowUs() {
+  return (
+    <section className="know-us">
+      <h1>Know Us</h1>
+    </section>
+  );
+}
+
+export { KnowUs };
