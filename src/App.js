b77d61566907a174f722ebca580aea1112726b09
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-07-11 13:22:24 -0500

    Scroll smooth

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 function App() {
   return (
     <React.Fragment>
-      <div className="screen">1</div>
-      <div className="screen">2</div>
-      <div className="screen">3</div>
-      <div className="screen">4</div>
-      <div className="screen">5</div>
-      <div className="screen">6</div>
+      <div className="container">
+        <section className="one">1</section>
+        <section className="two">2</section>
+        <section className="three">3</section>
+        <section className="four">4</section>
+        <section className="five">5</section>
+        <section className="six">6</section>
+      </div>
     </React.Fragment>
   );
 }
